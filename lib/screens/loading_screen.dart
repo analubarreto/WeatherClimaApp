49a9fa059585f630068d5a5a287e8e2f0cd412a3
Ana Luiza <analuibm@gmail.com>
2020-11-16 13:27:39 -0300

    added spinner to loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:clima/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,18 +19,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocationData() async {
     String apiKey = '225d07419871505f5c2fb80c4655304f';
-    double latitude = location.latitude;
-    double logitude = location.logitude;
     
-    NetworkHelper networkHelper = NetworkHelper(url: 'api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$logitude&appid=$apiKey');
+    NetworkHelper networkHelper = NetworkHelper(url: 'api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.logitude}&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
+
+    print(weatherData);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        child: SpinKitFoldingCube(
+          color: Colors.white,
+          size: 100.0
+        )
       ),
     );
   }
